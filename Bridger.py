b5f83b32f7c78a1ed0e0b48f6165703f152442b4
DynosusXD <pranjalthakur70024071@gmail.com>
2026-06-17 14:44:01 +0530

    v2 is live!
    Added:
    1. Option to choose language.
    2. Slot for API key in the code, if you don't wish to use Environment Variable.

--- Bridger.py
+++ Bridger.py
@@ -1,13 +1,15 @@
 from google import genai
 count = 1
-client = genai.Client()
+client = genai.Client(api_key="YourGeminiAPI")
+lang = input("Enter Language: ")
 while True:
     text = input("Enter Text: ")
+    
     if text == "0":
         break
     response = client.models.generate_content(
-        model="gemini-3.5-flash",
-        contents="Translate the following text to Vietnamese, or if its already vietnamese, then translate it to English. Do NOT add any other words, it should be a copy-pastable text. Text: " + text
+        model="gemini-2.5-flash-lite",
+        contents="Translate the following text to " + lang + ", or if its already " + lang + ", then translate it to English. Do NOT add any other words, it should be a copy-pastable text. Text: " + text
     )
     count += 1
     totest = response.text
